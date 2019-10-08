8abe6789253b0be1c7228ef90beb8bc4f6a841f7
Midnighter <midnighter@posteo.net>
2019-10-08 23:15:28 +0200

    feat: add a pydantic compound model

--- src/cobra_component_models/io/compound_model.py
+++ src/cobra_component_models/io/compound_model.py
@@ -4,7 +4,7 @@
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#     https://www.apache.org/licenses/LICENSE-2.0
+#    https://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -13,9 +13,18 @@
 # limitations under the License.
 
 
-"""Provide pydantic classes for (de-)serialization of components."""
+"""Provide a pydantic compound data model."""
 
+
+from typing import Optional
+
+from pydantic import Schema
 
 from .sbase_model import SBaseModel
-from .compartment_model import CompartmentModel
-from .compound_model import CompoundModel
+
+
+class CompoundModel(SBaseModel):
+    """Define a pydantic compound data model."""
+
+    charge: Optional[float] = Schema(None)
+    chemical_formula: Optional[str] = Schema(None, alias="chemicalFormula")
---
+++ tests/test_unit/io/test_compound_model.py
@@ -0,0 +1,53 @@
+# Copyright (c) 2019, Moritz E. Beber.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+"""Expect that compound models function as designed."""
+
+
+import pytest
+
+from cobra_component_models.io import CompoundModel
+
+
+def test_empty_init():
+    """Expect that a compound model can be instantiated without arguments."""
+    obj = CompoundModel()
+    assert obj.charge is None
+    assert obj.chemical_formula is None
+    assert obj.sbo_term is None
+    assert obj.notes is None
+    assert obj.names == []
+    assert obj.annotation == {}
+
+
+@pytest.mark.parametrize(
+    "attributes",
+    [
+        {"charge": -2.0},
+        {"chemical_formula": "C2H6O"},
+        {"sbo_term": "SBO:007"},
+        {"notes": "bla bla bla"},
+        {"names": ["one", "two", "three"]},
+        {"annotation": {"prefix": [("is", "one"), ("is", "two")]}},
+    ],
+)
+def test_init(attributes):
+    """Expect that the object is properly initialized."""
+    old_value = getattr(CompoundModel.Config, "allow_population_by_alias", False)
+    CompoundModel.Config.allow_population_by_alias = True
+    obj = CompoundModel(**attributes)
+    CompoundModel.Config.allow_population_by_alias = old_value
+    for attr, value in attributes.items():
+        assert getattr(obj, attr) == value
