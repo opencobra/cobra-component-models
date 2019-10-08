e4d270f3c5f1c0bb839811e059dc34231432713f
Midnighter <midnighter@posteo.net>
2019-10-08 23:07:19 +0200

    feat: add a pydantic compartment model

--- src/cobra_component_models/io/compartment_model.py
+++ src/cobra_component_models/io/compartment_model.py
@@ -4,7 +4,7 @@
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#     https://www.apache.org/licenses/LICENSE-2.0
+#    https://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -13,8 +13,13 @@
 # limitations under the License.
 
 
-"""Provide pydantic classes for (de-)serialization of components."""
+"""Provide a pydantic compartment data model."""
 
 
 from .sbase_model import SBaseModel
-from .compartment_model import CompartmentModel
+
+
+class CompartmentModel(SBaseModel):
+    """Define a pydantic compartment data model."""
+
+    pass
